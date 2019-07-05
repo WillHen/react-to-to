b92457c593831343950c231d2e54922389ded776
William <will.henshaw@eatstreet.com>
2019-07-05 12:06:22 -0500

    add date picker

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import './App.css';
-import axios from 'axios';
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from 'react-datepicker';
 
 window.id = 0;
 
 const Title = () => {
   return (
     <div>
-       <div>
-          <h1>to-do</h1>
-       </div>
+      <div>
+        <h1>to-do</h1>
+      </div>
     </div>
   );
-}
+};
 
-const TodoForm = ({ addTodo }) => {
+const TodoForm = ({ addTodo, setDate }) => {
   // Input tracker
   let input;
 
@@ -25,97 +26,96 @@ const TodoForm = ({ addTodo }) => {
           input = node;
         }}
       />
-      <button onClick={() => {
+      <button
+        onClick={() => {
           addTodo(input.value);
           input.value = '';
         }}
       >
-        +
       </button>
     </div>
   );
 };
 
-const Todo = ({todo, remove}) => {
+const Todo = ({ todo, remove, setDate }) => {
   // Each Todo
-  return (<li onClick={() => remove(todo.id)}>{todo.text}</li>);
-}
+  return (
+    <div>
+      <li onClick={() => remove(todo.id)}>{todo.text}</li>
+      <DatePickerToDo todo={todo}  setDate={setDate}/>
+     </div>
+      )
+};
 
-const TodoList = ({todos, remove}) => {
+const DatePickerToDo = ({ todo, setDate }) => {
+  return (
+    console.log(todo),
+    <DatePicker selected={todo.date} onChange={(date) => {setDate(todo, date)}} />
+  );
+};
+
+const TodoList = ({ todos, remove, setDate }) => {
   // Map through the todos
-  const todoNode = todos.map((todo) => {
-    return (<Todo todo={todo} key={todo.id} remove={remove}/>)
+  const todoNode = todos.map(todo => {
+    return <Todo todo={todo} key={todo.id} remove={remove} setDate={setDate}/>;
   });
-  return (<ul>{todoNode}</ul>);
-}
+  return <ul>{todoNode}</ul>;
+};
 
-
-
-class TodoApp extends React.Component{
-  constructor(props){
+class TodoApp extends React.Component {
+  constructor(props) {
     // Pass props to parent class
     super(props);
     // Set initial state
     this.state = {
       data: []
-    }
+    };
 
-    this.apiUrl = '//57b1924b46b57d1100a3c3f8.mockapi.io/api/todos';
   }
 
-    // Lifecycle method
-  componentDidMount(){
-    // Make HTTP reques with Axios
-    axios.get(this.apiUrl)
-      .then((res) => {
-        // Set state with result
-        this.setState({data:res.data});
-      });
-  }
+
   // Add todo handler
-  addTodo(val){
+  addTodo(val, time) {
     if (!val) {
       alert('To do must not be blank');
-
     } else {
       // Assemble data
-      const todo = {text: val, id: window.id++}
+      const todo = { text: val, id: window.id++, date: new Date() };
       // Update data
       this.state.data.push(todo);
       // Update state
-      this.setState({data: this.state.data});
-      
+      this.setState({ data: this.state.data });
     }
   }
+
+  setToDoDate(todo, val) {
+    todo.date = val;
+    this.setState({data: this.state.data})
+  }
   // Handle remove
-  handleRemove(id){
+  handleRemove(id) {
     // Filter all todos except the one to be removed
-    const remainder = this.state.data.filter((todo) => {
-      if(todo.id !== id) return todo;
+    const remainder = this.state.data.filter(todo => {
+      if (todo.id !== id) return todo;
     });
     // Update state with filter
-    this.setState({data: remainder});
+    this.setState({ data: remainder });
   }
 
-  render(){
+  render() {
     // Render JSX
     return (
       <div>
         <Title />
-        <TodoForm addTodo={this.addTodo.bind(this)}/>
-        <TodoList 
-          todos={this.state.data} 
+        <TodoForm addTodo={this.addTodo.bind(this)} />
+        <TodoList
+          todos={this.state.data}
           remove={this.handleRemove.bind(this)}
+          setDate={this.setToDoDate.bind(this)}
         />
       </div>
     );
   }
 }
 
-export default TodoApp
-
-
-
-
-
-
+export default TodoApp;
